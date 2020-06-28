e0fd6a83376ae5de1c00802e3a4543519a3107d3
Decker <deckerurbano@gmail.com>
2020-06-28 17:21:25 -0300

    add test and new master

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,3 +1,4 @@
-const url ='https://testwalletcolppy.herokuapp.com/';
+const url ='https://walletcolppy.herokuapp.com/';
+//const url ='https://testwalletcolppy.herokuapp.com/';
  //const url ='http://127.0.0.1:8000/';
 export default url;
